e021e56457b9b50d62c86ef9ee40e6ae3316a1cb
anushainugurthi <46727178+anushainugurthi@users.noreply.github.com>
2020-09-18 17:40:54 -0500

    Update Itemcard.js
    Updated Comments

--- src/components/Itemcard.js
+++ src/components/Itemcard.js
@@ -11,7 +11,6 @@ import TurmericBlackPepper from './TurmericBlackPepper';
 const Itemcard = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
